acbb5829b7024648c13c910c9879367891dd3123
Bryanjazo <bryanjazo88@gmail.com>
2021-02-11 12:29:28 -0800

    commit tree dd6f5f55011bd5c9f4d090f1cf712d72b0ca9899

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if end_with?("?")
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+    if end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+     self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
   end
 end
